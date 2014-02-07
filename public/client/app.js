b2bdf5eea7a8b8a1a22ae9ec40825f2bdee8f132
Patrick Ford <patrickford@msn.com>
2014-02-07 14:24:52 -0800

    Copied original files from shortly directory

---
+++ public/client/app.js
@@ -0,0 +1,60 @@
+window.Shortly = Backbone.View.extend({
+
+  template: _.template(' \
+      <h1>Shortly</h1> \
+      <div class="navigation"> \
+      <ul> \
+        <li><a href="#" class="index">All Links</a></li> \
+        <li><a href="#" class="create">Shorten</a></li> \
+      </ul> \
+      </div> \
+      <div id="search"> \
+        <form> \
+          <input class="search" type="text" name="search"> \
+          <input type="submit" value="Filter By"> \
+        </form> \
+      </div> \
+      <div id="container"></div>'
+  ),
+
+  events: {
+    "click li a.index":  "renderIndexView",
+    "click li a.create": "renderCreateView"
+  },
+
+  initialize: function(){
+    console.log( "Shortly is running" );
+    $('body').append(this.render().el);
+    this.renderIndexView(); // default view
+  },
+
+  render: function(){
+    this.$el.html( this.template() );
+    return this;
+  },
+
+  renderIndexView: function(e){
+    e && e.preventDefault();
+    $('#search').show();
+    var links = new Shortly.Links();
+    var linksView = new Shortly.LinksView( {collection: links} );
+    this.$el.find('#container').html( linksView.render().el );
+    this.updateNav('index');
+  },
+
+  renderCreateView: function(e){
+    e && e.preventDefault();
+    $('#search').hide();
+    var linkCreateView = new Shortly.LinkCreateView();
+    this.$el.find('#container').html( linkCreateView.render().el );
+    this.updateNav('create');
+  },
+
+  updateNav: function(className){
+    this.$el.find('.navigation li a')
+            .removeClass('selected')
+            .filter('.'+className)
+            .addClass('selected');
+  }
+
+});
