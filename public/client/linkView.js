ca52ae42c01a6f51bede73ae2a2878f0b0438551
Patrick Ford <patrickford@msn.com>
2014-02-08 10:35:06 -0800

    Included angular.min.js

--- public/client/linkView.js
+++ public/client/linkView.js
@@ -19,3 +19,19 @@ Shortly.LinkView = Backbone.View.extend({
   }
 
 });
+
+// <button ng-click="orderBy='created_at'">Created At</button>
+// <input type="text" ng-model="search" placeholder="search />"
+// <div class="links">
+//   <div class="link"
+//       ng-repeat="link in links | filter:search | orderBy:'visits' ">
+//       <img src="/redirect_icon.png"/> \
+//       <div class="info"> \
+//         <div class="visits"><span class="count">{{ link.visits }}</span>Visits</div> \
+//         <div class="title">{{ link.title }}</div> \
+//         <div class="original">{{ link.url }}</div> \
+//         <a href="{{ link.base_url }}/{{ link.code }}">{{ link.base_url }}/<%= code %></a> \
+//         <div class="lastvisit">Last visited at: {{ link.updated_at}} \
+//       </div>
+//   </div>
+// </div>
